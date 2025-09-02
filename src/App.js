076a5f7e528c643a0c310b0ad5792f0827a502bf
Jeet Shorey <132572002+jeetshorey123@users.noreply.github.com>
2025-09-03 00:18:07 +0530

    Clean setup: React truck optimizer app with proper Vercel configuration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TruckOptimizer from './components/TruckOptimizer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TruckOptimizer />
+    </div>
+  );
+}
+
+export default App;
